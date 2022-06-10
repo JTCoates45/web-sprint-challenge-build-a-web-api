371b98c26316a4e5f06646306032e712b130aa03
JT Coates <wyosred@yahoo.com>
2022-06-10 13:01:42 -0600

    return 404

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,95 @@
 // add middlewares here related to projects
+
+const projectsModel = require('./projects-model');
+
+const validateProjectId = (req, res, next) => {
+    projectsModel.get(req.params.id)
+    .then(result => {
+        if (result == null) {
+            next({
+                message: 'Project not found.',
+                status: 404
+            });
+            return;
+        }
+        req.project = result;
+        next();
+    })
+    .catch(next)
+}
+
+const validateProjectPost = (req, res, next) => {
+    let { name, description, completed } = req.body;
+    if (!name || typeof name !== 'string' || name.trim() === '') {
+        next({
+            message: 'Must provide name and description.',
+            status: 400
+        });
+        return;
+    } 
+    if (!description || typeof description !== 'string' || description.trim() === '') {
+        next({
+            message: 'Must provide name and description.',
+            status: 400
+        });
+        return;
+    }
+    if (completed) {
+        if (typeof completed !== 'boolean') {
+            next({
+                message: 'If providing completed status, it must be true or false.',
+                status: 400
+            });
+            return;
+        } else if (typeof completed === 'boolean') {
+            req.body = {
+                name: name,
+                description: description,
+                completed: completed
+            }
+            next();
+            return; 
+        }
+    }
+    req.body = {
+        name: name,
+        description: description
+    }
+    next();
+}
+
+const validateProjectUpdate = (req, res, next) => {
+    let { name, description, completed } = req.body;
+    if (!name || typeof name !== 'string' || name.trim() === '') {
+        next({
+            message: 'Must provide name and description.',
+            status: 400
+        });
+        return;
+    }
+    if (!description || typeof description !== 'string' || description.trim() === '') {
+        next({
+            message: 'Must provide name and description.',
+            status: 400
+        });
+        return;
+    }
+    if (completed === null || typeof completed !== 'boolean') {
+        next({
+            message: 'Must provide a completed status as true or false.',
+            status: 400
+        });
+        return;
+    }
+
+    name = name.trim();
+    description = description.trim();
+    req.body = { name, description, completed };
+    next();
+}
+
+module.exports = {
+    validateProjectId,
+    validateProjectPost,
+    validateProjectUpdate
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,52 @@
 // Write your "projects" router here!
+
+const express = require('express');
+const router = express.Router();
+const projectsModel = require('./projects-model');
+const { validateProjectId,
+        validateProjectPost,
+        validateProjectUpdate } = require('./projects-middleware');
+
+router.get('/', (req, res, next) => {
+   projectsModel.get()
+   .then(projectsArr => res.json(projectsArr))
+   .catch(next);
+})
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.json(req.project);
+})
+
+router.post('/', validateProjectPost, (req, res, next) => {
+    projectsModel.insert(req.body)
+    .then(newProject => res.status(201).json(newProject))
+    .catch(next);
+})
+
+router.delete('/:id', validateProjectId, (req, res, next) => {
+    projectsModel.remove(req.params.id)
+    .then(deleteCount => {
+        if (!deleteCount) {
+            next({
+                message: 'Project could not be deleted at this time.'
+            });
+            return;
+        }
+        res.status(200).end();
+    })
+    .catch(next)
+})
+
+router.put('/:id', validateProjectId, validateProjectUpdate, (req, res, next) => {
+    projectsModel.update(req.params.id, req.body)
+    .then(updatedProject => res.json(updatedProject))
+    .catch(next);
+})
+
+router.get('/:id/actions', validateProjectId, (req, res, next) => {
+    projectsModel.getProjectActions(req.params.id)
+    .then(actionsArr => res.json(actionsArr))
+    .catch(next);
+})
+
+module.exports = router;
