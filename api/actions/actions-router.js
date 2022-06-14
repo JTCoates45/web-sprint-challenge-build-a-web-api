5c41a142295b9213882cd37ec1fa1cc6be2851f7
JT Coates <wyosred@yahoo.com>
2022-06-13 20:25:05 -0600

    finished sprint

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,7 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const actionsModel = require('./actions-model');
-const { validateActionId } = require('./actions-middlware');
+const { validateActionId,
+        validateActionPost,
+        validateActionUpdate } = require('./actions-middlware');
 
 router.get('/', (req, res, next) => {
     actionsModel.get()
@@ -13,6 +15,32 @@ router.get('/', (req, res, next) => {
 
  router.get('/:id', validateActionId, (req, res) => {
     res.json(req.action);
+})
+
+router.post('/', validateActionPost, (req, res, next) => {
+    actionsModel.insert(req.body)
+    .then(newAction => res.status(201).json(newAction))
+    .catch(next);
+})
+
+router.delete('/:id', validateActionId, (req, res, next) => {
+    actionsModel.remove(req.params.id)
+    .then(deleteCount => {
+        if (!deleteCount) {
+            next({
+                message: 'Action could not be deleted at this time.'
+            });
+            return;
+        }
+        res.status(200).end();
+    })
+    .catch(next)
+})
+
+router.put('/:id', validateActionId, validateActionUpdate, (req, res, next) => {
+    actionsModel.update(req.params.id, req.body)
+    .then(updatedAction => res.json(updatedAction))
+    .catch(next);
 })
 
 module.exports = router;
